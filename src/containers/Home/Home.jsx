221b7dca3d57d841f178b40e1af238700830fa57
Aditya <adityathakur532@gmail.com>
2021-09-17 09:33:16 +0530

    banner added

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,8 +5,24 @@ function Home() {
     return (
         <div>
             <div className="container">
-                <div className="banner">Banner image</div>
-            </div> 
+                <div className="banner">
+                    <div className="quote">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quae labore magnam!
+                    </div>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={(event) => {
+                            event.preventDefault();
+                            history.push('/create-survey');
+                        }}
+                    >
+                        Create Survey
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
